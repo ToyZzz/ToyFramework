8a45a43b60dc53f128df615b820071fe56212b37
ToyZzz <584638298@qq.com>
2018-07-24 23:44:46 +0800

    version

--- UnityProj/Assets/Scripts/ToyFramework/ResourceModule/VersionBase.cs
+++ UnityProj/Assets/Scripts/ToyFramework/ResourceModule/VersionBase.cs
@@ -2,15 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VersionBase : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class Version : Singleton<Version>
+{
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
